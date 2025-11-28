7f95a4ee4a39f629a25d01adafaaa4771c8724b8
Cloudmg <120593203+Cloudmg@users.noreply.github.com>
2025-11-28 13:06:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Плавний скрол
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 80,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+    console.log("Armory Database Loaded.");
+});
